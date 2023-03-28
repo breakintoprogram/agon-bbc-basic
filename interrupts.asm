b3e7103d6e75321cff4ae1beb82b17af6c9662c8
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2023-03-28 17:38:58 +0100

    Added VBLANK_STOP, removed MB bodge introduced in previous change

--- interrupts.asm
+++ interrupts.asm
@@ -2,11 +2,12 @@
 ; Title:	BBC Basic for AGON - Interrupts
 ; Author:	Dean Belfield
 ; Created:	06/05/2022
-; Last Updated:	26/03/2023
+; Last Updated:	28/03/2023
 ;
 ; Modinfo:
 ; 23/03/2023:		Added VBLANK_INIT and VBLANK_HANDLER
 ; 26/03/2023:		Fixed bug - MB changing in interrupt
+; 28/03/2023:		Added VBLANK_STOP, removed MB bodge introduced in previous change
 
 			.ASSUME	ADL = 0
 				
@@ -17,6 +18,7 @@
 			SEGMENT CODE
 				
 			XDEF	VBLANK_INIT
+			XDEF	VBLANK_STOP
 			XDEF	VBLANK_HANDLER	
 
 			XREF	ESCSET	
@@ -26,16 +28,41 @@
 ; Hook into the MOS VBLANK interrupt
 ;
 VBLANK_INIT:		DI
-			LD		E, 32h				; Set up the VBLANK interrupt
-			LD		HL, VBLANK_HANDLER	
+
+			LD		A, MB 				; Get a 24-bit pointer to
+			LD		HL, VBLANK_HANDLER		; this interrupt handler routine who's
+			CALL		SET_AHL16 			; address is a 16-bit pointer in BBC BASIC's segment
+
+			LD		E, 32h				; Set up the VBlank Interrupt Vector
 			MOSCALL		mos_setintvector
-			EX		DE, HL				; DEU: Address of previous interrupt handler
-			LD		HL, VBLANK_HANDLER_JP + 1	; This is a 16-bit address within this segment
-			LD		A, MB	 			; Get the current segment
-			CALL		SET_AHL16 			; Convert to an absolute 24-bit address
+
+			PUSH.LIL	HL				; HLU: Pointer to the MOS interrupt vector
+			POP.LIL		DE 				; DEU: Pointer to the MOS interrupt vector
+			
+			LD		HL, VBLANK_HANDLER_JP + 1	; Pointer to the JP address in this segment
+			LD		A, MB	 			; Get the segment BBC BASIC is running in
+			LD		(VBLANK_HANDLER_MB + 1), A 	; Store in the interrupt handler
+			CALL		SET_AHL16 			; Convert pointer to an absolute 24-bit address
 			LD.LIL		(HL), DE			; Self-modify the code
 			EI	
 			RET
+
+; Unhook the custom VBLANK interrupt
+;
+VBLANK_STOP:		DI
+			LD		HL, VBLANK_HANDLER_JP + 1	; Pointer to the JP address in this segment
+			LD		A, (VBLANK_HANDLER_MB + 1)	; The stored MB of the segment BBC BASIC is running in
+			PUSH		AF 				; Stack the MB for later
+			CALL		SET_AHL16			; Convert pointer to an absolute 24-bit address
+			LD.LIL		DE, (HL)			; DEU: Address of MOS interrupt vector
+			PUSH.LIL	DE				; Transfer to HL
+			POP.LIL		HL
+			LD		E, 32h
+			MOSCALL		mos_setintvector		; Restore the MOS interrupt vector
+			POP		AF 				; Restore MB to this segment
+			LD		MB, A 
+			EI
+			RET 
 
 ; Set the MSB of HL (U) to A
 ;
@@ -69,7 +96,7 @@ VBLANK_HANDLER:		DI
 			PUSH		IX
 			LD		A, MB
 			PUSH		AF 
-			LD		A, 4				; TODO: This is a bit of a bodge
+VBLANK_HANDLER_MB:	LD		A, 0				; This is self-modified by VBLANK_INIT
 			LD		MB, A
 			CALL.LIS	DO_KEYBOARD
 			POP		AF
